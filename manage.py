97c7f6b92c7a4ba6c290df1841451766e17bd8a5
Ross Crawford-d'Heureuse <ross.crawford@kumukan.com>
2015-01-16 10:10:48 +0100

    added command to update_playlist

--- manage.py
+++ manage.py
@@ -4,13 +4,14 @@ from flask.ext.script import Manager
 from flask.ext.assets import ManageAssets
 
 from chute import app
-from chute.commands import Workers, Register
+from chute.commands import Workers, Register, UpdatePlaylist
 
 manager = Manager(app)
 
 manager.add_command('workers', Workers())
 manager.add_command('assets', ManageAssets())
 manager.add_command('register', Register())
+manager.add_command('update_playlist', UpdatePlaylist())
 
 if __name__ == "__main__":
     manager.run()
