b6746ba219498d4512a60e79a10bb916285effd4
Ross Crawford-d'Heureuse <rosscdh@s.local>
2016-01-07 17:26:59 +0100

    overrode so update_playlist works with wordpress if constant is defined

--- chute/commands/update_playlist.py
+++ chute/commands/update_playlist.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from flask.ext.script import Command, Option
 
+import config as settings
+
 from ..services import BoxApiService
 
 
@@ -9,7 +11,11 @@ class UpdatePlaylist(Command):
         """
         """
         s = BoxApiService()
-        resp = s.update_playlist()
+        if getattr(settings, 'WORDPRESS_RSS_BASE_URL', None) is not None:
+            resp = s.get_rss_from_wordpress()
+            resp = s.update_playlist(content=resp)
+        else:
+            resp = s.update_playlist()
         print(resp)
         s.download_feed()
 
